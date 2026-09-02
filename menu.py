79e0f8c27b6b7cae2ce53c3a283429db3ad5aa17
Igna01 <nachomiranda1311@gmail.com>
2026-09-02 10:56:39 -0300

    arreglos menu

--- menu.py
+++ menu.py
@@ -4,12 +4,17 @@ def opciones_menu():
     print("                 MENU DE CONTENIDOS")
     print("-" * 50)
     print("Seleccione una opcion:")
-    print("1. Consultar Line Up")
-    print("2. Buscar artista")
-    print("3. Registrar asistencia")
-    print("4. Modificar asistencia")
-    print("5. Consultar informes")
-    print("6. Modificar ranking de artistas")
+    print("1. Asignar / Modificar Line Up")
+    print("2. Consultar Line Up")
+    print("3. Buscar artista")
+    print("4. Registrar asistencia")
+    print("5. Modificar asistencia")
+    print("6. Consultar informe de artista con mayor convocatoria")
+    print("7. Consultar informe de asistencia total por dia")
+    print("8. Consultar informe de top 3 de artistas con mayor convocatoria")
+    print("9. Consultar informe de shows que superaron la capacidad")
+    print("10. Consultar informe de top 3 de franjas horarias con mayor asistencia")
+    print("11. Mostrar ranking de artistas")
     print("0. Salir")
     print("-" * 50)
     
@@ -17,8 +22,8 @@ def menu():
     opciones_menu()
     opcion = int(input("Opción: "))
     # Validar que la opcion elegida este dentro de los rangos permitidos
-    while opcion < 0 or opcion > 6:
-        print("Error, la opcion elegida debe estar entre 0 y 6")
+    while opcion < 0 or opcion > 11:
+        print("Error, la opcion elegida debe estar entre 0 y 11")
         opciones_menu()
         opcion = int(input("Seleccione una opcion: "))
     return opcion
@@ -28,21 +33,36 @@ def main():
     while opcion != 0:
         if opcion == 1:
             print()
-            # funciones.consultarLineUp()
+            # funciones.asignarLineUp()
         elif opcion == 2:
             print()
-            # funciones.buscarArtista()
+            # funciones.consultarLineUp()
         elif opcion == 3:
             print()
-            # funciones.registrarAsistencia()
+            # funciones.buscarArtista()
         elif opcion == 4:    
             print()
-            # funciones.modificarAsistencia
+            # funciones.registrarAsistencia()
         elif opcion == 5:
             print()
-            # funciones.consultarInformes()
-            # PARA MI HAY QUE SEPARAR TODOS LOS INFORMES
-            #prueba
-    
+            # funciones.modificarAsistencia()
+        elif opcion == 6:
+            print()
+            # funciones.consultarArtistaMayorConvocatoria()
+        elif opcion == 7:
+            print()
+            # funciones.consultarAsistenciaTotalPorDia()
+        elif opcion == 8:
+            print()
+            # funciones.consultarTop3ArtistasMayorConvocatoria()
+        elif opcion == 9:
+            print()
+            # funciones.consultarShowsSobrepasaronCapacidad()
+        elif opcion == 10:
+            print()
+            # funciones.consultarTop3FranjasHorariasMayorAsistencia()
+        elif opcion == 11:
+            print()
+            # funciones.mostrarRankingArtistas()
 
 main()
